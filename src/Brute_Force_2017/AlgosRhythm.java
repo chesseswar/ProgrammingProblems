267b2eac40f39ce6970bcd48d594f686c45236de
chesseswar <chesseswar@gmail.com>
2018-08-05 20:04:32 -0500

    finished a bunch of problems

--- src/Brute_Force_2017/AlgosRhythm.java
+++ src/Brute_Force_2017/AlgosRhythm.java
@@ -12,30 +12,23 @@ public class AlgosRhythm {
         int[] notes = new int[]{1,2,4};
         while (numCase-- > 0){
             int[] dp = new int[in.nextInt()+1];
-            Arrays.fill(dp,1);
-            dp[2] = 2;
-            for (int i = 1; i < notes.length; i++){
-                for (int j = 1; j < dp.length; j++){
-                    if (j >= notes[i]){
-                        dp[j] = sum(dp,j);
+            if (dp.length < 3){
+                System.out.println(1);
+            } else {
+                Arrays.fill(dp,1);
+                dp[2] = 2;
+                dp[3] = 3;
+                dp[4] = 6;
+                for (int i = 1; i < notes.length; i++){
+                    for (int j = 4; j < dp.length; j++){
+                        if (j >= notes[i]){
+                            dp[j] = dp[j-1] + dp[j-2] + dp[j-4];
+                        }
                     }
                 }
+
+                System.out.println(dp[dp.length-1]);
             }
-
-            System.out.println(dp[dp.length-1]);
         }
-    }
-
-    public static int sum(int[] arr, int index){
-        int sum = 0;
-        if (index == 2){
-            return arr[0] + arr[1];
-        } else if (index == 1){
-            return arr[0];
-        }
-        for (int i = index-3; i < index; i++){
-            sum += arr[i];
-        }
-        return sum;
     }
 }
--- src/First_Bytes_Advanced_2016/PostalDelivery.java
+++ src/First_Bytes_Advanced_2016/PostalDelivery.java
@@ -9,7 +9,30 @@ public class PostalDelivery {
         //Scanner in = new Scanner(System.in);
         int numCase = in.nextInt();
         while (numCase-- > 0){
+            int vertices = in.nextInt();
+            int edges = in.nextInt();
+            HashMap<Integer,Integer> map = new HashMap<>();
+            for (int i = 1; i <= vertices; i++){
+                map.put(i,0);
+            }
 
+            for (int i = 0; i < edges; i++){
+                int a = in.nextInt();
+                map.put(a,map.get(a)+1);
+                int b = in.nextInt();
+                map.put(b,map.get(b)+1);
+            }
+            System.out.println(even(map) ? "yes" : "no");
         }
+    }
+
+    public static boolean even(HashMap<Integer,Integer> map){
+        for (int i : map.keySet()){
+            if (map.get(i) % 2 != 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
