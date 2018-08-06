17191c06431f3e86ee6fa417b35a93d4bdb5108b
chesseswar <chesseswar@gmail.com>
2018-08-05 20:31:15 -0500

    commented "Alien Sort"

--- src/First_Bytes_Advanced_2016/AlienSort.java
+++ src/First_Bytes_Advanced_2016/AlienSort.java
@@ -8,18 +8,18 @@ public class AlienSort {
         Scanner in = new Scanner(new File("input.txt"));
         //Scanner in = new Scanner(System.in);
 
-        int words = in.nextInt();
-        int count = 1;
+        int words = in.nextInt(); //number of words in test case
+        int count = 1; //for output "year 1", "year 2", etc
         while (words != 0){
-            String input = in.next();
-            HashMap<Character,Character> cypher = new HashMap<>();
+            String input = in.next(); //inputted word
+            HashMap<Character,Character> cypher = new HashMap<>(); //map new alphabet to old alphabet in this hashmap
             for (int j = 0; j < input.length(); j++){
                 cypher.put(input.charAt(j),(char)(j+65));
             }
 
-            HashMap<String,String> translations = new HashMap<>();
+            HashMap<String,String> translations = new HashMap<>(); //map altered strings to original strings in this
             String[] translated = new String[words];
-            for (int i = 0; i < words; i++){
+            for (int i = 0; i < words; i++){ //for each word, swap each char using cypher hashmap
                 String word = in.next();
                 char[] letters = word.toCharArray();
                 for (int j = 0; j < letters.length; j++){
@@ -29,13 +29,13 @@ public class AlienSort {
                 translations.put(translated[i],word);
             }
 
-            Arrays.sort(translated);
+            Arrays.sort(translated); //sort the array of altered strings
             System.out.println("year " + count);
-            for (int i = 0; i < translated.length; i++){
+            for (int i = 0; i < translated.length; i++){ //print out the original strings in the order of the altered ones
                 System.out.println(translations.get(translated[i]));
             }
 
-            words = in.nextInt();
+            words = in.nextInt(); //get next set of words
             count++;
         }
     }
