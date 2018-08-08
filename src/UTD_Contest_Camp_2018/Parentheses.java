91227b15531fa1da168f5d260d275a1fdc06c562
chesseswar <chesseswar@gmail.com>
2018-08-08 11:14:14 -0500

    parentheses problem finished (for leetcode now)

--- src/UTD_Contest_Camp_2018/Parentheses.java
+++ src/UTD_Contest_Camp_2018/Parentheses.java
@@ -8,13 +8,35 @@ public class Parentheses {
         Scanner in = new Scanner(System.in);
         while (in.hasNext()){
             int input = in.nextInt();
-            answer(input, "(", 1, 0);
+            Answer a = new Answer(input);
         }
     }
 
-    public static void answer(int input, String soFar, int numOpen, int numClose){
+    public static List<String> generateParenthesis(int n){
+        Answer a = new Answer(n);
+        String[] answer = a.answer.split(" ");
+        List<String> list = new ArrayList<>();
+        for (String str : answer){
+            list.add(str);
+        }
+
+        return list;
+
+    }
+}
+
+class Answer {
+    int input;
+    String answer;
+    public Answer(int i){
+        input = i;
+        answer = "[\n";
+        answer(input,"(",1,0);
+    }
+
+    public void answer(int input, String soFar, int numOpen, int numClose){
         if (soFar.length() == input * 2){
-            System.out.println(soFar + " " + check(soFar));
+            answer += (soFar + " ");
             return;
         }
 
@@ -22,7 +44,7 @@ public class Parentheses {
             for (int i = soFar.length(); i < input*2; i++){
                 soFar += ")";
             }
-            System.out.println(soFar + " " + check(soFar));
+            answer += (soFar + " ");
             return;
         }
         if (numOpen > numClose){
@@ -30,22 +52,5 @@ public class Parentheses {
         }
 
         answer(input,soFar+"(",numOpen+1,numClose);
-    }
-
-    public static boolean check(String par){
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        for (char c : par.toCharArray()){
-            if (stack.size() == 0 && c == ')'){
-                return false;
-            }
-
-            if (c == '('){
-                stack.push(' ');
-            } else {
-                stack.pop();
-            }
-        }
-
-        return stack.size() == 0;
     }
 }
